f804cef4b4e290f552a455e3e89ac13d082cadd3
Sérgio Dantas <sergiodnts828@gmail.com>
2025-09-22 09:15:42 -0300

    IDMa - 1.1 - question A

---
+++ Provas/IDMa - 1.1.lean
@@ -0,0 +1,17 @@
+set_option pp.parens true
+
+section A
+
+/-
+Basta demonstrar _.                       : Prop → Cmd
+_ nasceu na capital de _.                 : Person ⨯ Country → Prop
+Se João é _, então _ ∈ _.                 : (Person → Prop) ⨯ Person ⨯ Set Person → Prop
+O computador de _ tem _ teclas quebradas. : Person ⨯ Nat → Prop
+Para cada x : Set _, defina x' ≝ _.       : Type ⨯ (Set Int → Set Int) → Cmd
+Seja x : _ tal que √2.                    : Type Error
+_ ∈ {{√2}, {_, _}}.                       : Set Real ⨯ (Real ⨯ Real) → Prop
+n ∣ m ⇐≝⇒ (∃_ : Int)[_].                  : Var ⨯ Prop → Cmd
+n _ 42 ⇐⇒ _.                              : (Int ⨯ Int → Prop) ⨯ Prop → Prop
+-/
+
+end A
