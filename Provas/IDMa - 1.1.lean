c0225bfcf132c5683229a31db54b7b1cc596ed8b
Sérgio Dantas <sergiodnts828@gmail.com>
2025-09-22 13:47:31 -0300

    proved: C2

--- Provas/IDMa - 1.1.lean
+++ Provas/IDMa - 1.1.lean
@@ -24,25 +24,90 @@ theorem conj_as_neg_disj (P Q : Prop) : P ∧ Q → ¬(¬P ∨ ¬Q) := by
   intro h' h''
   rcases h' with ⟨p, q⟩
   rcases h'' with np | nq
-  . apply np
+  · apply np
     assumption
-  . apply nq
+  · apply nq
     assumption
 
 -- B2
 theorem curry_uncurry (P Q R : Prop) : (P ∧ Q → R) ↔ (P → (Q → R)) := by
   constructor
-  . intro h p q
+  · intro h p q
     have pq : P ∧ Q := by
       constructor
-      . assumption
-      . assumption
+      · assumption
+      · assumption
     apply h
     assumption
-  . intro h pq
+  · intro h pq
     rcases pq with ⟨p, q⟩
     have qr : Q → R := h p
     have r : R := qr q
     assumption
 
 end B
+
+section C
+
+/-
+Allowed tactics:
+
+- Int.add_assoc             Int.mul_assoc
+- Int.add_zero              Int.mul_one
+- Int.add_comm              Int.mul_comm
+- Int.add_right_neg         Int.add_mul
+-/
+
+-- Lemmas
+
+theorem neg_neg (a : Int) : -(-a) = a := by
+  calc
+    -(-a) = -(-a) + 0             := by rw [Int.add_zero]
+    _     = -(-a) + (a + (-a))    := by rw [Int.add_right_neg]
+    _     = (-(-a) + a) + (-a)    := by rw [Int.add_assoc]
+    _     = (a + (-(-a))) + (-a)  := by rw [Int.add_comm a (-(-a))]
+    _     = a + (-(-a) + (-a))    := by rw [Int.add_assoc]
+    _     = a + (-a + (-(-a)))    := by rw [Int.add_comm (-(-a)) (-a)]
+    _     = a + 0                 := by rw [Int.add_right_neg]
+    _     = a                     := by rw [Int.add_zero]
+
+theorem mul_zero (a : Int) : a * 0 = 0 := by
+  calc
+    a * 0 = a * 0 + 0                     := by rw [Int.add_zero]
+    _     = a * 0 + (a * 0 + (-(a * 0)))  := by rw [Int.add_right_neg]
+    _     = (a * 0 + a * 0) + (-(a * 0))  := by rw [Int.add_assoc]
+    _     = (0 * a + 0 * a) + (-(a * 0))  := by rw [Int.mul_comm]
+    _     = ((0 + 0) * a) + (-(a * 0))    := by rw [Int.add_mul]
+    _     = 0 * a + (-(a * 0))            := by rw [Int.add_zero]
+    _     = a * 0 + (-(a * 0))            := by rw [Int.mul_comm]
+    _     = 0                             := by rw [Int.add_right_neg]
+
+theorem neg_one_mul (a : Int) : (-1) * a = -a := by
+  calc
+    (-1) * a = (-1) * a + 0               := by rw [Int.add_zero]
+    _        = (-1) * a + (a + (-a))      := by rw [Int.add_right_neg]
+    _        = ((-1) * a + a) + (-a)      := by rw [Int.add_assoc]
+    _        = ((-1) * a + a * 1) + (-a)  := by rw [Int.mul_one]
+    _        = ((-1) * a + 1 * a) + (-a)  := by rw [Int.mul_comm a 1]
+    _        = (((-1) + 1) * a) + (-a)    := by rw [Int.add_mul]
+    _        = ((1 + (-1)) * a) + (-a)    := by rw [Int.add_comm 1 (-1)]
+    _        = (0 * a) + (-a)             := by rw [Int.add_right_neg]
+    _        = (a * 0) + (-a)             := by rw [Int.mul_comm]
+    _        = 0 + (-a)                   := by rw [mul_zero]
+    _        = (-a) + 0                   := by rw [Int.add_comm]
+    _        = -a                         := by rw [Int.add_zero]
+
+-- C2
+theorem neg_sub (a b : Int) : -(a - b) = b - a := by
+  calc
+    -(a - b) = -(a + (-b))             := by rw [Int.sub_eq_add_neg]  -- Sugar
+    _        = (-1) * (a + (-b))       := by rw [neg_one_mul]
+    _        = (a + (-b)) * (-1)       := by rw [Int.mul_comm]
+    _        = a * (-1) + (-b) * (-1)  := by rw [Int.add_mul]
+    _        = (-1) * a + (-1) * (-b)  := by rw [Int.mul_comm a (-1), Int.mul_comm (-b) (-1)]
+    _        = (-a) + (-(-b))          := by rw [neg_one_mul, neg_one_mul]
+    _        = (-a) + b                := by rw [neg_neg]
+    _        = b + (-a)                := by rw [Int.add_comm]
+    _        = b - a                   := by rw [Int.sub_eq_add_neg]  -- Sugar
+
+end C
